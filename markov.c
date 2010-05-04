916a41ec3452916e3929d1577a47332869e1f02d
Frederik Deweerdt <frederik.deweerdt@gmail.com>
2010-05-04 15:55:40 +0200

    Cleaner hash interface

--- markov.c
+++ markov.c
@@ -13,12 +13,37 @@
 #define ASCII_CHARS 255
 
 struct markov {
-	int graph[ASCII_CHARS][ASCII_CHARS];
-	int graph_total[ASCII_CHARS];
-	int init[ASCII_CHARS];
-	int init_total;
+	int _graph[ASCII_CHARS][ASCII_CHARS];
+	int _graph_total[ASCII_CHARS];
+	int _init[ASCII_CHARS];
+	int _init_total;
 	int token_length;
 };
+
+static int hash_graph_elems(struct markov *m)
+{
+	return ARRAY_SIZE(m->_graph[0]);
+}
+
+static void hash_graph_links_set(struct markov *m, unsigned char cur, unsigned char next, int value)
+{
+	m->_graph[cur][next] = value;
+}
+
+static int hash_graph_links_get(struct markov *m, unsigned char cur, unsigned char next)
+{
+	return m->_graph[cur][next];
+}
+
+static void hash_graph_total_set(struct markov *m, unsigned char c, int i)
+{
+	m->_graph_total[c] = i;
+}
+
+static int hash_graph_total_get(struct markov *m, unsigned char c)
+{
+	return m->_graph_total[c];
+}
 
 static unsigned char markov_get_ponderated(struct markov *m, unsigned char cur, int i)
 {
@@ -26,22 +51,38 @@ static unsigned char markov_get_ponderated(struct markov *m, unsigned char cur, 
 	int total;
 
 	total = 0;
-	for (n = 0; n < ARRAY_SIZE(m->graph[0]); n++) {
-		total += m->graph[cur][n];
+	for (n = 0; n < hash_graph_elems(m); n++) {
+		total += hash_graph_links_get(m, cur, n);
 		if (i < total)
 			return n;
 	}
 	return 0;
 }
+
+static int hash_init_size(struct markov *m)
+{
+	return ARRAY_SIZE(m->_init);
+}
+
+static void __attribute__((unused)) hash_init_occurs_set(struct markov *m, unsigned char c, int i)
+{
+	m->_init[c] = i;
+}
+
+static int hash_init_occurs_get(struct markov *m, unsigned char c)
+{
+	return m->_init[c];
+}
+
 unsigned char markov_get_next(struct markov *m, unsigned char cur)
 {
 	int i;
 
 	/* No letters following this one */
-	if (m->graph_total[cur] == 0)
+	if (hash_graph_total_get(m, cur) == 0)
 		return 0;
 
-	i = rand() % m->graph_total[cur];
+	i = rand() % hash_graph_total_get(m, cur);
 
 	return markov_get_ponderated(m, cur, i);
 }
@@ -51,20 +92,23 @@ unsigned char markov_get_init(struct markov *m)
 	int i;
 
 	do {
-		i = rand() % ARRAY_SIZE(m->init);
-	} while(m->init[i] == 0);
+		i = rand() % hash_init_size(m);
+	} while(hash_init_occurs_get(m, i) == 0);
 
 	return i;
 }
 
 void markov_add_cycle(struct markov *m, unsigned char cur, unsigned char next)
 {
-	m->graph[cur][next]++;
+	int i;
+
+	i = hash_graph_links_get(m, cur, next);
+	hash_graph_links_set(m, cur, next, i + 1);
 }
 
 void markov_add_init_state(struct markov *m, unsigned char cur)
 {
-	m->init[cur]++;
+	m->_init[cur]++;
 }
 
 struct markov *markov_create(int token_length)
@@ -86,17 +130,18 @@ void markov_freeze(struct markov *m)
 	int total;
 
 	total = 0;
-	for (i = 0; i < ARRAY_SIZE(m->init); i++) {
-		total += m->init[i];
+	for (i = 0; i < hash_init_size(m); i++) {
+		total += hash_init_occurs_get(m, i);
 	}
-	m->init_total = total;
+	m->_init_total = total;
 
-	for (i = 0; i < ARRAY_SIZE(m->graph); i++) {
+	/* Sum up all the graph items */
+	for (i = 0; i < ARRAY_SIZE(m->_graph); i++) {
 		total = 0;
-		for (j = 0; j < ARRAY_SIZE(m->graph[i]); j++) {
-			total += m->graph[i][j];
+		for (j = 0; j < hash_graph_elems(m); j++) {
+			total += hash_graph_links_get(m, i, j);
 		}
-		m->graph_total[i] = total;
+		hash_graph_total_set(m, i, total);
 	}
 }
 
